773444768ab79e519c972f851963078f098b7a8e
zanfertech <38507641+zanfertech@users.noreply.github.com>
2018-06-12 08:03:39 -0700

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,16 +1,16 @@
 class profile::agent_nodes {
-  host {'centosmysql01':
-    ensure => present,
-    ip     => 192.168.50.208,
-  }
-  host {'cnetosmysql02':
-    ensure => present,
-    ip     => 192.168.50.24,
-  }
-  host {'centosmysql03':
-    ensure => present,
-    ip     => 192.168.50.73,
-  }
+#  host {'centosmysql01':
+#    ensure => present,
+#    ip     => 192.168.50.208,
+#  }
+#  host {'cnetosmysql02':
+#    ensure => present,
+#    ip     => 192.168.50.24,
+#  }
+#  host {'centosmysql03':
+#    ensure => present,
+#    ip     => 192.168.50.73,
+#  }
   host {'centospuppetslave':
     ensure => present,
     ip     => 192.168.50.27,
