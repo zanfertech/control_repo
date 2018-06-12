fd7e6864c24101a955e7a16804d3c294978c3f5f
zanfertech <38507641+zanfertech@users.noreply.github.com>
2018-06-12 13:12:01 -0700

    Update galera_cluster.pp

--- site/profile/manifests/galera_cluster.pp
+++ site/profile/manifests/galera_cluster.pp
@@ -22,7 +22,7 @@ class profile::galera_cluster {
   
   file {'/etc/my.cnf':
     ensure => file,
-    source => 'puppet:///modules/my.cnf',
+    source => 'puppet:///modules/mysql-wsrep/my.cnf',
     require => Package['mysql-wsrep-5.7'],
   }
   
