c010ad412adb096493844d918e6229e26c9881ff
zanfertech <38507641+zanfertech@users.noreply.github.com>
2018-06-12 08:42:20 -0700

    Update db.pp

--- site/profile/manifests/db.pp
+++ site/profile/manifests/db.pp
@@ -1,2 +1,14 @@
 class profile::db {
+
+  package {'mysql-wsrep':
+    ensure => installed,
+    status => running,
+  }
+  
+  file {'/etc/my.cnf':
+    ensure => present,
+    requires => Package(mysql-wsrep),
+  }
+  
+  
 }
