855ac5274c08e881e7ee70d54a8a53ff135e1656
zanfertech <38507641+zanfertech@users.noreply.github.com>
2018-06-12 13:27:01 -0700

    Update init.pp

--- site/mysql-wsrep/manifests/init.pp
+++ site/mysql-wsrep/manifests/init.pp
@@ -1,2 +1,33 @@
 class mysql-wsrep {
+  include profile::mysql_wsrep_yum_repo
+  include profile::galera_yum_repo
+  include profile::rsync
+
+  exec { 'upgrade':
+    command => "/usr/bin/yum upgrade -y mysql-wsrep-libs-compat-5.7",
+    unless  => "/usr/bin/rpm -qa | grep mysql-wsrep",
+    require => Yumrepo['mysql-wsrep'],
+  }
+
+  package {'mysql-wsrep-5.7':
+    ensure => installed,
+    require => Yumrepo['mysql-wsrep'],
+  }
+  
+  package {'galera-3':
+    ensure => installed,
+    require => Yumrepo['galera'],
+  }
+  
+  file {'/etc/my.cnf':
+    ensure => file,
+    source => 'puppet:///modules/mysql-wsrep/my.cnf',
+    require => Package['mysql-wsrep-5.7'],
+  }
+  
+  service {'mysqld':
+    ensure => 'running',
+    enable => 'true',
+    require => File['/etc/my.cnf'],
+  }
 }
