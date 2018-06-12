efc58fd37ed1e5a68fefba65b8eccdc2f354c453
zanfertech <38507641+zanfertech@users.noreply.github.com>
2018-06-12 13:28:02 -0700

    Update galera_cluster.pp

--- site/profile/manifests/galera_cluster.pp
+++ site/profile/manifests/galera_cluster.pp
@@ -1,39 +1,3 @@
 class profile::galera_cluster {
-
-  include profile::mysql_wsrep_yum_repo
-  include profile::galera_yum_repo
-  include profile::rsync
   include mysql-wsrep
-
-  exec { 'upgrade':
-    command => "/usr/bin/yum upgrade -y mysql-wsrep-libs-compat-5.7",
-    unless  => "/usr/bin/rpm -qa | grep mysql-wsrep",
-    require => Yumrepo['mysql-wsrep'],
-  }
-
-  package {'mysql-wsrep-5.7':
-    ensure => installed,
-    require => Yumrepo['mysql-wsrep'],
-  }
-  
-  package {'galera-3':
-    ensure => installed,
-    require => Yumrepo['galera'],
-  }
-  
-  file {'/etc/my.cnf':
-    ensure => file,
-    source => 'puppet:///modules/mysql-wsrep/my.cnf',
-    require => Package['mysql-wsrep-5.7'],
-  }
-  
-  service {'mysqld':
-    ensure => 'running',
-    enable => 'true',
-    require => File['/etc/my.cnf'],
-  }
 }
-  
-  
-  
-  
