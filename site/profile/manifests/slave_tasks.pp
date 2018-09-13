d2d416a1066552d2fd0b38d458b781172770a74a
zanfertech <38507641+zanfertech@users.noreply.github.com>
2018-09-12 22:41:16 -0700

    Update slave_tasks.pp
    Added DHCP server

--- site/profile/manifests/slave_tasks.pp
+++ site/profile/manifests/slave_tasks.pp
@@ -2,4 +2,12 @@ class profile::slave_tasks{
   package {'vim':
     ensure => installed,
   }
+  
+  package {'dnsmasq':
+    ensure => installed,
+  }
+  
+  package {'dnsmasq-utils':
+    ensure => installed,
+  }
 }
